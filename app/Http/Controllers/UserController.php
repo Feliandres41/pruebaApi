66ab54ccc78cc286e1635988d92de5f6faffcac1
andres <feliandres412@gmail.com>
2026-06-02 17:11:01 -0500

    prueba

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Users;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|string|max:255|unique:users,name',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = Users::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuario creado correctamente',
+            'user' => $user,
+        ], 201);
+    }
+}
